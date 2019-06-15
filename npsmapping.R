70b668c02d54f3d42fef81af4ea7b309113f9542
sestaszak <staszak.sara@gmail.com>
2019-06-14 17:58:31 -0700

    map updates

--- npsmapping.R
+++ npsmapping.R
@@ -4,18 +4,26 @@ library(rgeos)
 library(sp)
 require(maptools)
 
+np<-get(load("NPS_dat_byPark.Rda"))
+npcodes<-unique(np$`Park Code`)
 # area <- readShapePoly("data/nps_boundary/nps_boundary.shp")
 ### use rgdal::readOGR or sf::st_read
-area<-readOGR("data/nps_boundary/nps_boundary.shp")
+#area<-readOGR("data/National_Park_Service__Park_Unit_Boundaries/National_Park_Service__Park_Unit_Boundaries.shp")
+area<-readOGR("data/National_Park_Service__Park_Unit_Centroids/National_Park_Service__Park_Unit_Centroids.shp")
 
-area.points<-fortify(area)
+centnps<-area[area@data$UNIT_CODE%in%npcodes,]
 
+#area.points<-fortify(area)
 
-mapusa<-get_map(location="united states", zoom=3)
+### google maps doesn't work and location not as bbox calls google maps
+#mapusa<-get_map(location="united states", zoom=3, source = "stamen", maptype="toner")
+usbbox<-c(left = -125.76 , bottom = 18.44, right= -63.19, top=50.598)
+mapusa<-get_stamenmap(bbox=usbbox, zoom=3, maptype = "toner")
 
-ggmap(mapusa) + geom_polygon(aes(x=long, y=lat, group=group), data=area.points, alpha=0.5)
+##runs slowly on macbook:
+#ggmap(mapusa) + geom_polygon(aes(x=long, y=lat, group=group), data=area.points, alpha=0.5)
 
-plot_usmap(regions="states") + geom_polygon(aes(x=long, y=lat, group=group, color="green"), data=area.points)
+#plot_usmap(regions="states") + geom_polygon(aes(x=long, y=lat, group=group, color="green"), data=area.points)
 
 
 ###flag campsites/campgrounds by park
